934f8d59f03bf34d4fb592e83f06f0b48dcd05fe
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-02-13 21:30:12 +0530

    add configuration

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import Registration from './src/pages/Registration/Registration'
+
+export default function App() {
+  return (
+   
+      <View>
+        <Text>Student Management App</Text>
+        <Registration/>
+      </View>
+
+   
+
+  )
+}
