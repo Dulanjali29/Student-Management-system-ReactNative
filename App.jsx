cad5fff4b177a00833d31503ff7129947da55e93
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-02-17 14:12:58 +0530

    configuration in navigation start

--- App.jsx
+++ App.jsx
@@ -6,19 +6,22 @@ import Registration from './src/pages/Registration/Registration'
 import Login from './src/pages/Login/Login'
 import StudentSearch from './src/pages/StudentSearch/StudentSearch'
 import StudentAction from './src/pages/StudentAction/StudentAction'
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-   
-      <View>
-       <Login/>
-        {/* <Registration/> */}
-        {/* <StudentSearch/> */}
-        {/* <StudentAction/> */}
-      </View>
+    <NavigationContainer>
+      <Login />
+    </NavigationContainer>
+    // <View>
+    //  
+    //   {/* <Registration/> */}
+    //   {/* <StudentSearch/> */}
+    //   {/* <StudentAction/> */}
+    // </View>
 
 
-   
+
 
   )
 }
